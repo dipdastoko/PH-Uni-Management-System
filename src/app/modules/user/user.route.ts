05bdc882aa87ed0252c2294559f2d3f422afefd7
rpamgi <dipdastoko@gmail.com>
2025-02-25 10:46:42 +0600

    middleware added in user route.ts

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,13 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserContollers } from './user.controller';
 
 const router = express.Router();
 
-router.post('/create-student', UserContollers.createStudent);
+const senaBahini = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  next();
+};
+
+router.post('/create-student', senaBahini, UserContollers.createStudent);
 
 export const UserRoutes = router;
